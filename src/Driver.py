b7e9691e73f34fef1a91c65a101fc80526be561b
James Ting <tingjames2000@outlook.com>
2020-05-14 14:35:24 -0700

    Added more data points

--- src/Driver.py
+++ src/Driver.py
@@ -6,17 +6,17 @@ def main():
     f = open(api_key_location,"r")
     api_key = f.readline()
     f.close()
-    ON_DESKTOP = False
+    ON_DESKTOP = True
     
     if(ON_DESKTOP):
-        training_data_location = "C:\\Users\\James Ting\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\datasets\\training_data.csv"
+        training_data_location = "C:\\Users\\UserD\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\datasets\\training_data.csv"
     else:
         training_data_location = "C:\\Users\\James Ting\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\datasets\\training_data.csv"
     
     
-    starting_summ_name = "mymainisahri0321" #Seed Summoner
+    starting_summ_name = "Ben Rhodes" #Seed Summoner
     
-    num_data_points = 30
+    num_data_points = 100
     data_set_maker = DataSetMaker(api_key_location,region,training_data_location,starting_summ_name,num_data_points=num_data_points)
     data_set_maker.makeTrainingData()
 
