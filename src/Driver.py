f46826eafaffb5dd63b9175b77a7386451f95069
James <tingjames2000@outlook.com>
2020-06-01 00:43:05 -0700

    Reached 10 014 data points

--- src/Driver.py
+++ src/Driver.py
@@ -7,57 +7,61 @@ import io
 import time
 import random
 
-   
-#method that makes a data
-def makeData(training_data_location,writeToColumns = False):
+
+# method that makes a data
+def makeData(training_data_location, writeToColumns=False):
     region = 'na1'
     api_key_location = "api_key.txt"
-    f = open(api_key_location,"r")
+    f = open(api_key_location, "r")
     api_key = f.readline()
     f.close()
-    
-    
 
     num_data_points = 3
     num_data_batches = 120
 
-    #get the set of current match ID's
+    # get the set of current match ID's
     matchID_list = set()
-    with open(training_data_location,"r") as f:
+    with open(training_data_location, "r") as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
             matchID_list.add(row[0])
         starting_matchID = row[0]
     f.close()
-    match_iterator = MatchCrawler(api_key,region,starting_matchID,max_iterations=num_data_batches*num_data_batches)
-    data_set_maker = DataSetMaker(api_key_location,region,training_data_location,match_iterator)
+    match_iterator = MatchCrawler(
+        api_key, region, starting_matchID, max_iterations=num_data_batches*num_data_batches)
+    data_set_maker = DataSetMaker(
+        api_key_location, region, training_data_location, match_iterator)
     if(writeToColumns):
         writeColumns()
-    
+
     setNewMatchID = data_set_maker.setNewStartingID
     sleep = time.sleep
     makeData = data_set_maker.makeTrainingData
-   
-   #collect the data
+
+   # collect the data
     for batch in range(num_data_batches):
         try:
-            starting_matchID = makeData(num_data_points,starting_matchID)
+            starting_matchID = makeData(num_data_points, starting_matchID)
             matchID_list.add(starting_matchID)
             sleep(45)
         except:
-            starting_matchID = random.sample(matchID_list,1)[0]
+            starting_matchID = random.sample(matchID_list, 1)[0]
             sleep(60)
 
-#method that makes a validate data object and returns the result
+# method that makes a validate data object and returns the result
+
+
 def validateData(training_data_location):
     validator = DataSetValidator(training_data_location)
     validator.validateDataSet()
 
+
 def writeColumns():
-    f = open("C:\\Users\\James Ting\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\docs\\columns.csv","w")
+    f = open("../docs/columns.csv", "w")
     f.write(str(DataSetMaker.columns))
     f.close()
+
 
 def main():
     ON_DESKTOP = False
@@ -65,7 +69,7 @@ def main():
     WRITE_TO_COLUMNS = False
 
     api_key_location = "api_key.txt"
-    f = open(api_key_location,"r")
+    f = open(api_key_location, "r")
     api_key = f.readline()
     f.close()
 
@@ -73,13 +77,12 @@ def main():
         training_data_location = "C:\\Users\\UserD\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\datasets\\training_data.csv"
     else:
         training_data_location = "C:\\Users\\James Ting\\OneDrive - McGill University\\Personal\\Personal Projects\\LoL-Predictor\\datasets\\training_data.csv"
-    
+
     if(VALIDATE_DATA):
         validateData(training_data_location)
     else:
-        makeData(training_data_location,WRITE_TO_COLUMNS)
+        makeData(training_data_location, WRITE_TO_COLUMNS)
         validateData(training_data_location)
-        
 
 
 if __name__ == "__main__":
